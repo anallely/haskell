4571736c4fd448ed2e125cdb44a685c6138a895d
Anallely <anaoko@gmail.com>
2016-07-19 23:13:46 -0500

    More on pattern matching notes

--- patternMatching.hs
+++ patternMatching.hs
@@ -49,4 +49,53 @@ third (_, _, z) = z
 --ghci>>third ("ana", 23, 'h')
 --'h'
 
+-- PATTERN MATCHING ON LISTS AND LIST COMPREHENSIONS
+-- x:xs pattern is used often 
+--We want to bind something to several variables, so we use ()
+
+head' :: [a] -> a
+head' [] = error "No head on empty lists"
+head' (x:_) = x    
+-- ghci>>head' []
+-- *** Exception: No head on empty lists
+-- CallStack (from HasCallStack):
+--   error, called at patternMatching.hs:56:12 in main:Main
+-- ghci>>head [1,2,3,4]
+-- 1
+
+verbose :: (Show a) => [a] -> String
+verbose [] = "This is an empty list"
+verbose (x:[]) = "This list has only one element: " ++ show x
+verbose (x:y:[]) = "This list has two elements: " ++ show x ++ "and " ++ show y
+verbose (x:y:_) = "This list has more than two elements, but the first and second are " ++ show x ++ "and " ++ show y
+
+
+friendOfThree :: [a] -> String
+friendOfThree(x:y:z:[]) = "Yes i am"
+-- ghci>>friendOfThree [1,2,3,4]
+-- "*** Exception: patternMatching.hs:74:1-36: Non-exhaustive patterns in function friendOfThree
+-- 
+-- ghci>>friendOfThree [1,2,3]
+-- "Yes i am"
+
+-- AS PATTERNS
+-- It is a pattern matching where you can continue make reference to the original whole item
+-- xs@(x,y,_)
+firstLetter :: String -> String
+firstLetter "" = "empty :("
+firstLetter all@(x:xs) = "The first letter for " ++ all ++ " is " ++ [x]
+-- ghci>>firstLetter ""
+-- "empty :("
+-- ghci>>firstLetter "Anita lava la tina"
+-- "The first letter for Anita lava la tina is A"
+
+
+
+
+
+
+
+
+
+
 
